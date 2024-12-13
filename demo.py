64ec5b3f818423bea9ed11a74855391ad0878a10
Nikhil <viswanathnikhil44@gmail.com>
2024-12-13 14:13:27 +0530

    added required files

--- demo.py
+++ demo.py
@@ -2,17 +2,89 @@
 
 # ins = MongoDBClient()
 
-from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig
-from us_visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+# from us_visa.configuration.aws_connection import S3Client
 
-from us_visa.components.data_ingestion import DataIngestion
-from us_visa.components.data_validation import DataValidation
 
-di_ins = DataIngestion(DataIngestionConfig)
+# ins = S3Client()
 
-di_art = di_ins.initiate_data_ingestion()
 
-dv_ins = DataValidation(data_ingestion_artifact=di_art ,data_validation_config=DataValidationConfig)
 
-dv_art = dv_ins.initiate_data_validation()
+# from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig
+# # from us_visa.entity.artifact_entity import DataIngestionArtifact
+# from us_visa.components.data_ingestion import DataIngestion
+# from us_visa.components.data_validation import DataValidation
+# from us_visa.components.data_transformation import DataTransformation
+# from us_visa.components.model_trainer import ModelTrainer
+# from us_visa.components.model_evaluation import ModelEvaluation
+# from us_visa.components.model_pusher import ModelPusher
 
+# di_ins = DataIngestion(DataIngestionConfig)
+
+# di_art = di_ins.initiate_data_ingestion()
+
+# dv_ins = DataValidation(data_ingestion_artifact=di_art ,data_validation_config=DataValidationConfig)
+
+# dv_art = dv_ins.initiate_data_validation()
+
+# dt_ins = DataTransformation(data_ingestion_artifact=di_art,data_transformation_config=DataTransformationConfig,
+#                             data_validation_artifact=dv_art)
+
+
+# dt_art =dt_ins.initiate_data_transformation()
+
+# mt_ins = ModelTrainer(data_transformation_artifact=dt_art,model_trainer_config=ModelTrainerConfig)
+
+# mt_art = mt_ins.initiate_model_trainer()
+
+# me_ins = ModelEvaluation(model_eval_config=ModelEvaluationConfig,data_ingestion_artifact=di_art,model_trainer_artifact=mt_art)
+
+# me_art = me_ins.initiate_model_evaluation()
+
+# mp_ins = ModelPusher(me_art,ModelPusherConfig)
+
+# mp_art = mp_ins.initiate_model_pusher()
+
+
+# from us_visa.pipeline.training_pipeline import TrainPipeline
+
+# tr_pp = TrainPipeline()
+
+# tr_pp.run_pipeline()
+
+from us_visa.pipeline.prediction_pipeline import USvisaData,USvisaClassifier
+
+
+# usvisa_data = USvisaData(
+#                         continent= 'Asia',
+#                         education_of_employee = 'Doctorate',
+#                         has_job_experience = 'Y',
+#                         requires_job_training = 'N',
+#                         no_of_employees= 20000,
+#                         company_age= 100,
+#                         region_of_employment = 'Northeast',
+#                         prevailing_wage= 10000,
+#                         unit_of_wage= 'Year',
+#                         full_time_position= 'Y',
+#                         )
+
+usvisa_data = USvisaData(
+                        continent= 'Asia',
+                        education_of_employee = 'Doctorate',
+                        has_job_experience = 'N',
+                        requires_job_training = 'Y',
+                        no_of_employees= 15000,
+                        company_age= 10,
+                        region_of_employment = 'West',
+                        prevailing_wage= 1000,
+                        unit_of_wage= 'Hour',
+                        full_time_position= 'N',
+                        )
+
+
+usvisa_df = usvisa_data.get_usvisa_input_data_frame()
+
+model_predictor = USvisaClassifier()
+
+value = model_predictor.predict(dataframe=usvisa_df)[0]
+
+print(value)
--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 DB_NAME = 'US_VISA'
 
 COLLECTION_NAME= "visa_data"
@@ -12,6 +13,16 @@ FILE_NAME: str = "usvisa.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+MODEL_FILE_NAME = "model.pkl"
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "eu-north-1"
+
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
@@ -28,3 +39,27 @@ Data Validation realted contant start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "us-visaaa"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 9000
